8b62d0be7fd88b44e69014406882d9a5dfe34cda
richard tsang <richard.tsang9641@gmail.com>
2023-02-17 20:50:48 +0800

    temp work

--- components/App.js
+++ components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Card from './Card.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world!</h1>
+        <Card/>
       </div>
     );
   }
---
+++ components/Card.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../styles/card.scss";
+// class Card extends Component {
+//   render() {
+//     return (
+//       <div className="card">
+//         123
+//       </div>
+//     );
+//   }
+// }
+const Card = (props) => {
+  return <div className="card">
+    <div className="name">Richard Tsang</div>
+  </div>;
+};
+
+export default Card;
