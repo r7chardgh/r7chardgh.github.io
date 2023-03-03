b130ad303fc8eec02748d79c3f0996c64acd1f78
richard tsang <richard.tsang9641@gmail.com>
2023-03-03 17:46:09 +0800

    update skill card and style

--- components/App.js
+++ components/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 
+import Skill from './Skill'
+
 const App = () => {
   return (
     <>
       <Grid container spacing={0} >
         <Grid item xs={2} md={1}></Grid>
         <Grid item xs={2} md={3}>
+          
         </Grid>
         <Grid item xs={8} md={8}>
           <div className="name"><div className="name--first">richard</div><div className="name--last">tsang</div></div>
           <div className="greet">i'm a <span className="role">frontend</span><div className="role role--sec-row">developer</div></div>
+          <Skill/>
         </Grid>
       </Grid>
     </>
--- components/Skill.js
+++ components/Skill.js
@@ -1,12 +1,62 @@
 import React, { useState } from "react";
 
-import Grid from "@mui/material/Grid";
+import "../styles/skill.scss";
 
 const App = () => {
+  const skills = [
+    {
+      level: 0,
+      cat: [
+        {
+          title: "frontend web dev.",
+          item: ["HTML", "CSS", "JavaScript"],
+        },
+      ],
+    },
+    {
+      level: 1,
+      cat: [
+        {
+          title: "programming",
+          item: ["Node.js"],
+        },
+        {
+          title: "design",
+          item: ["Figma"],
+        },
+        {
+          title: "framework",
+          item: ["React", "React Native", "Next.js"],
+        },
+      ],
+    },
+  ];
   return (
-    <>
-    
-    </>
+    <div className="skill-card">
+      <div className="title">skills</div>
+      {skills.map((e) => {
+        return (
+          <div className="flex-row">
+            <div className="item-container">
+            {e.cat.map((e) => {
+              return (
+                <div className="item-wrapper">
+                  <div className="title--sub">{e.title}</div>
+                  {e.item.map((e) => {
+                    return <div className="item">{e}</div>;
+                  })}
+                </div>
+              );
+            })}
+            </div>
+            <div class="line"></div>
+            <div className="vt">{e.level === 0 ? "familiar" : "capable"}</div>
+          </div>
+        );
+      })}
+
+      <div className=""></div>
+    </div>
   );
 };
 
